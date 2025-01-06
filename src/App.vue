4bd3ada126a4978e0e4892c3370407c2d02b33ef
ramon chuinca <ramonchuinca@hotmail.com>
2025-01-06 17:15:38 -0400

    comessando um novo teste

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ComponenteTeste from './components/ComponenteTeste.vue';
+import Dados from './components/Dados.vue';
+
+</script>
+
+
+<template>
+  <header>TESTE-Vue-Js</header>
+  <Dados/>
+  <ComponenteTeste/>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/ComponenteTeste.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>
+            PRIMEIRO COMPONETE TESTE
+        </h1>
+    </div>
+</template>
+
+
+
+<style scoped>
+    div{
+        background-color: #0e7e28;
+        padding: 1rem;
+        margin: 30px;
+        text-align: center;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    div:hover{
+     background-color:#13b939;
+    }
+
+    h1{
+        color: white;
+        margin-bottom: 10px;
+    }
+
+</style>
+
+
+
+<script setup>
+
+</script>
